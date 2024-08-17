e3a154d0f1fff9d804954b3daddfe4b0db0a31e8
Tainted-Fool <lazaro1herrera@gmail.com>
2024-08-16 19:56:10 -0700

    Found missing plugin
    LSPOutline was the missing plugin
    Made stow directory for nvim

--- nvim/AppData/Local/nvim/init.lua
+++ nvim/AppData/Local/nvim/init.lua
@@ -1,6 +1,6 @@
 -- Init.lua is the entry point for nvim
 if (vim.g.vscode) then
-    -- VScode settings
+    -- VScode Settings
     require("core.vscode")
     vim.keymap.set("n", "[<Space>", "O<Esc>j", { desc = "Create Space Above", noremap = true, silent = true })
     vim.keymap.set("n", "]<Space>", "o<Esc>k", { desc = "Create Space Below", noremap = true, silent = true })
@@ -13,6 +13,6 @@ else
     -- require("core.utils") -- testing
     -- User Autocommands
     require("core.autocommands")
-    -- User Plugins
-    require("core.lazy")
+    -- User Plugins Manger
+    require("core.lazyman")
 end
