bbe4a6eeaf71b21dc74a77e96e026d8a11688d51
Tainted-Fool <lazaro1herrera@gmail.com>
2026-02-11 18:44:09 -0800

    Add headings highlight to kanagawa
    Markdown headings now have color
    Configure markview settings
    Edit tmux insert character bug

--- nvim/.config/nvim/lua/plugins/markview.lua
+++ nvim/.config/nvim/lua/plugins/markview.lua
@@ -1,23 +1,39 @@
 return {
     -- Markdown viewer
     "oxy2dev/markview.nvim",
-    lazy = true,
-    priority = 49,
-    opts = {
-        code_blocks = {
-            style = "simple", -- simple | minimal | language
-            language_direction = "left", -- left | right
-            position = "overlay", -- inline | overlay
-            pad_char = " ",
-            pad_amount = 2,
-            border_hl = "dark",
-            min_width = 70,
-            sign = true,
-        },
-        markdown = {
-            list_items = {
-                shift_amount = 2,
+    lazy = false,
+    -- priority = 49,
+    config = function()
+        local markview = require("markview")
+        local presets = require("markview.presets")
+        markview.setup({
+            markdown = {
+                enable = true,
+                block_quotes = {
+                    enable = true,
+                    wrap = true,
+                },
+                code_blocks = {
+                    enable = true,
+                    border_hl = "MarkviewCode",
+                    info_hl = "MarkviewCodeInfo",
+                    label_direction = "right", -- left | right
+                    label_hl = nil,
+                    min_width = 60,
+                    pad_amount = 2,
+                    pad_char = " ",
+                    style = "block", -- simple | block
+                    sign = true,
+                },
+                list_items = {
+                    enable = true,
+                    wrap = true,
+                    shift_width = 4,
+                },
+                headings = presets.headings.marker, -- glow | glow_center | slanted | arrowed | simple | marker
+                horizontal_rules = presets.horizontal_rules.arrowed, -- thin | thick | double | dashed | dotted | solid | arrowed
+                tables = presets.tables.rounded, -- none | single | double | rounded | solid
             }
-        }
-    }
+        })
+    end
 }
